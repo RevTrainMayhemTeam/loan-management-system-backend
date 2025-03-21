f63b40eb8a45f273ad422d0bb0c7d0da7738a04e
Juan-ae <juan.avarca.0301@gmail.com>
2025-03-21 13:21:50 -0600

    modified the LoanDto

--- src/main/java/com/mayhem/lms/controller/LoanController.java
+++ src/main/java/com/mayhem/lms/controller/LoanController.java
@@ -1,8 +1,10 @@
 package com.mayhem.lms.controller;
 
+import com.mayhem.lms.dto.GetLoanDto;
+import com.mayhem.lms.model.Loan;
 import com.mayhem.lms.service.LoanServiceImpl;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/loans")
@@ -11,5 +13,10 @@ public class LoanController {
 
     public LoanController(LoanServiceImpl loanServiceImpl) {
         this.loanServiceImpl = loanServiceImpl;
+    }
+
+    @PostMapping
+    public ResponseEntity<GetLoanDto> createLoan(@RequestBody Loan newLoan){
+
     }
 }
