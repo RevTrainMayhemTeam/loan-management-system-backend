c89a364196bb78dcaea21415b8831d42bf945f28
Luis Momox <lumomox@gmail.com>
2025-03-27 11:47:57 -0600

    added logging to AuthController and UserController for better traceability; improved error handling and validation messages

--- src/main/java/com/mayhem/lms/controller/UserController.java
+++ src/main/java/com/mayhem/lms/controller/UserController.java
@@ -57,31 +57,41 @@ public class UserController {
     }
 
     /**
-     * Update user details: first name, last name and phone number
+     * Update user details: first name, last name and phone number. Only user can update its own details
      * @param id
      * @param userDetails
      * @return
      */
     @PutMapping("/{id}")
-    public ResponseEntity<GetUserDto> updateUser(@PathVariable Long id, @RequestBody User userDetails, HttpSession session) {
+    public ResponseEntity<?> updateUser(@PathVariable Long id, @RequestBody User userDetails, HttpSession session) {
         GetUserDto userLogged = (GetUserDto) session.getAttribute("user");
         if (userLogged == null) {
-            logger.info("User not logged in");
+            logger.error("Not logged in");
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         }
-        if (userLogged.getId() == userDetails.getId()) {
-            logger.info("User logged in: {}, user details: {}", userLogged.getId(), userDetails.getId());
 
-            if (userDetails.getFirstName() == null || userDetails.getFirstName().trim().isEmpty())
-                return ResponseEntity.badRequest().build();
-            if (userDetails.getLastName() == null || userDetails.getLastName().trim().isEmpty())
-                return ResponseEntity.badRequest().build();
+        //Check if userId matches with logged user
+        if (userLogged.getId() == id) {
+            logger.info("Updating user with id {}", id);
+
+            //Validations to check no empty fields are sent
+            if (userDetails.getFirstName() == null || userDetails.getFirstName().trim().isEmpty()) {
+                logger.error("First name must not be empty");
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("First name must not be empty");
+            }
+            if (userDetails.getLastName() == null || userDetails.getLastName().trim().isEmpty()) {
+                logger.error("Last name must not be empty");
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("First name must not be empty");
+            }
+            //Attempt to update user
             GetUserDto updatedUser = userService.updateUser(id, userDetails);
             if (updatedUser == null)
-                return ResponseEntity.notFound().build();
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
+            logger.info("User with id {} updated", id);
             return ResponseEntity.ok(updatedUser);
         }
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        logger.info("Unauthorized access");
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Unauthorized access");
     }
 
     @DeleteMapping("/{id}")
@@ -89,11 +99,13 @@ public class UserController {
         GetUserDto userLogged = (GetUserDto) session.getAttribute("user");
 
         boolean deleted = userService.deleteUser(id, userLogged);
-
+        logger.info("Deleting user with id: {}", id);
         if (deleted){
             session.invalidate();
+            logger.info("User with id {} deleted", id);
             return ResponseEntity.ok("User successfully deleted");
         }else{
+            logger.info("Cannot delete user: {} invalid credentials", id);
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Not the right credentials :(");
         }
     }
